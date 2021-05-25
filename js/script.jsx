570fcf19d76e1120b8b0eb4e578a3801434b8d6c
Renzo Garcia <77386862+lassrenzo@users.noreply.github.com>
2021-05-25 17:29:38 +0800

    Update script.jsx

--- js/script.jsx
+++ js/script.jsx
@@ -1,85 +1,125 @@
-class FormLogin extends React.Component {
-  render() {
-    return ( 
-    <div>
-     <input type="email" class="form-control form-control-lg" placeholder=" Phone Number / Username / Email" autofocus/>
-     </div>
-    )}
-}
-class FormPassword extends React.Component {
-  render() {
-    return (<input type="password" class="form-control form-control-lg mt-2" placeholder=" Password " />
-    );
-  }
-}
+var destination = document.getElementById('react-container')
+class Note extends React.Component {
 
-class LoginButton extends React.Component {
-  render() {
-    return ( <div>
-    <button type="submit" class="btn btn-lg btn-danger login-btn">Log In</button>
-    <a href="" class="login-link text-primary">Forgot Password</a>
-    <a href="" class="login-link text-primary">Log in with SMS</a>   
-    <hr/>
+    constructor(props){
+        super(props);
+        this.state = {
+            editing: false
+        }
+    }
+    componentWillMount = () => {
+        this.style = {
+            right: this.randomBetween( 0, window.innerWidth - 150, 'px'),
+            top: this.randomBetween( 0, window.innerHeight - 150, 'px')
+        }
+    }
+    randomBetween =( x, y, s ) =>{
+        return( x + Math.ceil(Math.random() *(y-x)) ) + 5
+    }
+    
+    edit = () => {
+        this.setState({ editing: true })
+    }
+    
+    save = () => {
+        this.props.onChange(this.refs.newText.value, this.props.id)
+        this.setState({editing: false})
+     }
+    
+    delete = (id) => {
+        this.props.onRemove(this.props.id)
+    }
+    
+    renderForm = () => {
+        return (
+        <div className="note" style={this.style}>
+        <textarea ref="newText" placeholder="New Note!" className='form-control'></textarea>
+        <button onClick={this.save} className='btn btn-success btn-sm glyphicon glyphicon-floppy-disk'>Save</button> 
     </div>
-    )}
-}
+        )
+    
+    }
+    
+    renderDisplay = () => {
+        return (
+        <div className="note" style={this.style}>
+               <p>{this.props.children}</p>
+               <span>
+                <button onClick={this.edit} className='btn btn-primary fas fa-pencil'> Edit</button>
+                <button onClick={this.delete} className='btn btn-danger fas fa-trash'> X</button>
+                </span>
+            </div>
+            )   
+    }
+    
+    render(){
+       return( <ReactDraggable>{
+           (this.state.editing ) ? this.renderForm() : this.renderDisplay()      
+       }</ReactDraggable>)
+       }
+    }
 
-class LoginWith extends React.Component {
-  render() {
-    return (
-    <div class="login-with d-flex justify-content-around">
-      <a href="" type="button" class="btn-lg btn-primary" target="_blank">
-          <i class="fab fa-facebook sm-icon"> Facebook</i>
-      </a>
-
-      <a href="" type="button" class="btn-lg btn-primary" target="_blank">
-          <i class="fab fa-google sm-icon"> Google</i>
-      </a>
-
-      <a href="" type="button" class="btn-lg btn-dark" target="_blank">
-          <i class="fab fa-apple sm-icon"> Apple</i>
-      </a>
-</div>);
-  }
-}
-
-class NewAccount extends React.Component {
-  render() {
-    return (
-    <div class="new-account text-muted">
-<p>New to Shopee?
-<a href="" class="new-link text-danger"> Sign Up</a>   
-</p>
-</div>);
-  }
-}
-
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer>
-      <div class="container-footer">
-     <p> © 2021 Shopee. All Rights Reserved</p>
-<p>Country & Region: Singapore Indonesia Taiwan Thailand Malaysia Vietnam Philippines Brazil México</p>
-</div>
-  </footer>);
-  }
-}
   
-// VARIABLE FOR JSX REACT
-var destination = document.getElementById('app');
-
-ReactDOM.render(
-// WHAt
-<div class="card container" id="login-form">
-<FormLogin/><br />
-<FormPassword/><br />
-<LoginButton/>
-<LoginWith/>
-<NewAccount/>
-</div>,
-// WHERE
-// THIS IS CODE FOR DESTINATION VARIABLE
-
-  destination
-);
+    class Board extends React.Component {
+    
+    constructor(props){
+        super(props); 
+            this.state = {
+            notes: []
+        }
+    }
+    
+    nextId = () => {
+        this.uniqueId = this.uniqueId || 0 
+        return this.uniqueId++
+    }
+    
+    add = (text) => {
+        var notes = [
+            ...this.state.notes,
+            {
+                id: this.nextId(),
+                note: text
+            }
+        ]
+        this.setState({notes})
+    }
+    
+    update = ( newText, id ) => {
+        var notes = this.state.notes.map(
+            note => ( note.id !== id) ?
+                note:
+                {
+                    ...note,
+                    note: newText 
+                }
+        )
+        this.setState({notes})
+    }
+    
+    remove = (id) => {
+        var notes = this.state.notes.filter(note => note.id !== id) 
+        this.setState({notes})
+    }
+    
+    eachNote = ( note ) => {
+        return (<Note key={note.id}
+                        id={note.id}
+                        onChange={this.update}
+                        onRemove={this.remove}>{note.note}</Note>)
+    }
+    
+    render(){
+        return(
+        <div className="board">
+            {this.state.notes.map(this.eachNote)}
+            <button class="btn btn-sm glyphicon glyphicon-plus btn-success far fa-plus" onClick={()=>this.add("New Note!")}></button>
+    </div>
+        )
+    }
+    }
+    ReactDOM.render( 
+        <div>
+           <Note> </Note> 
+        <Board count={10}/>
+    </div>, destination)
